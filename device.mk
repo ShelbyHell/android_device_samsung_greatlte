97db0fa5fc9cabaa1cddcfb42c084ad8062afa93
Tim Zimmermann <tim@linux4.de>
2021-09-10 20:02:14 +0200

    fixup! greatlte: Use the group Wi-Fi Direct interface on bcm43xx
    * Required with WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY
    
    Change-Id: Ia978d7d38d59ae003a3a5507f043033b2626721d
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -340,3 +340,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
