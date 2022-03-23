242ae0e00f6ef493368ef53358b1090f12302d54
ivanmeler <i_ivan@windowslive.com>
2022-03-23 18:00:22 +0000

    universal8895-common: Uprev cam provider to 2.5
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -74,15 +74,8 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.5-service
     camera.universal8895 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@1.0 \
-    android.hardware.camera.device@3.2 \
-    android.hardware.camera.device@3.3 \
-    android.hardware.camera.device@3.4 \
-    android.hardware.camera.provider@2.4
 
 # Camera configurations
 PRODUCT_COPY_FILES += \
