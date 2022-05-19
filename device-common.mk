7e17af28dbcd642b5a759f9e40a96577d1b547b6
ivanmeler <i_ivan@windowslive.com>
2022-05-19 18:21:22 +0000

    universal8895-common: disable hwc with a shim
    This shim could also be used to fix it
    https://android.googlesource.com/platform/hardware/google/graphics/common/+/6935a9b0ac01b978519d8d704dee3994440ff023/libhwc2.1/libdevice/ExynosLayer.cpp#350

--- device-common.mk
+++ device-common.mk
@@ -281,7 +281,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Shims
 PRODUCT_PACKAGES += \
     libaudioparams_shim \
-    libexynoscamera_shim
+    libexynoscamera_shim \
+    libexynosdisplay_shim
 
 # stagefright
 PRODUCT_PACKAGES += \
