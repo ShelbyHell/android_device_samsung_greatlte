398044558378ca055b7e37a304538b081290280e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-29 09:29:32 +0200

    universal8895-common: Switch to two-stage init mounting
    * Stop handling vendor mounting from kernel, first stage init is gonna
      do that, together with system (which is now /system)
    * Use correct mount flags coming from kernel dts
    * Move system and vendor mount points to /dev/block/by-name/, we don't
      have bootdevice symlink to that point in init
    * Copy fstab to recovery for first stage init
    * drop recoveryonly on /efs.
    
    Change-Id: Iaa0ecc3f13c3cfd3fe357ca509549d0f68a29cf0
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -8,6 +8,15 @@ LOCAL_SRC_FILES		:= etc/fstab.samsungexynos8895
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.samsungexynos8895_ramdisk
+LOCAL_MODULE_STEM  := fstab.samsungexynos8895
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.samsungexynos8895
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
